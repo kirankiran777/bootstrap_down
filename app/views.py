4b2cff80b0cf5608995dca873f9d0f52e887c468
kirankiran7777 <kirankannemadugu777@gmail.com>
2022-06-29 18:29:19 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def bootstrap_down(request):
     return render(request,'bootstrap_down.html')
+
+def cdn(request):
+    return render(request,'cdn.html')
--- project13/urls.py
+++ project13/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bootstrap_down/',bootstrap_down,name='bootstrap_down'),
+    path('cdn/',cdn,name='cdn'),
 ]
